2f83e1f229d4601682f9fa1c0822d82b16f0dd6e
Naman13Kumawat <naman.kumawat07@gmail.com>
2022-11-27 19:38:34 +0530

    Code Push

---
+++ Backend/utils/email.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    from: "doctalkpracticum@gmail.com",
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: "DOcTalk Verify <doctalkpracticum@gmail.com>",
+    to: ["ojashgupta2103@gmail.com"], //Mail to admin, to verify Doctor
+    subject: options.subject,
+    text: options.message,
+    attachments: options.attachment,
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+  console.log("Email Sent");
+};
+
+module.exports = sendEmail;
